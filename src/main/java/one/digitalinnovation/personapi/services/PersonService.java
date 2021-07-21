153d5326cc8319821f778c424fefae8e4621a0de
Marco Grilli <marco.grilli15@gmail.com>
2021-07-21 11:01:03 -0300

    Alterando mensagem ID novamente

--- src/main/java/one/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -52,7 +52,7 @@ public class PersonService {
         Person updatedPerson = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(updatedPerson);
 
-        MessageResponseDTO messageResponse = createMessageResponse("ID de pessoa atualizado com sucesso! ", savedPerson.getId());
+        MessageResponseDTO messageResponse = createMessageResponse("ID de pessoa atualizado com sucesso!", savedPerson.getId());
 
         return messageResponse;
     }
